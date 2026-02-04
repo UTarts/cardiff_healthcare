41bdb968b53b53c78afd7ec82ecad403addd99c5
Utkarsh Tripathi <utarts.in@gmail.com>
2026-02-04 13:55:48 +0530

    New update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
             <img 
               src="/logo.webp" 
               alt="Cardiff Healthcare" 
-              className="h-10 w-auto object-contain" 
+              className="h-14 w-auto object-contain" 
             />
           </Link>
 
